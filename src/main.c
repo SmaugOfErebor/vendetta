2e970177340acea27b473315d301e07f8a7a3ef6
Justin Nelson <jnelson@iol.unh.edu>
2026-05-04 16:18:23 -0400

    Making the input requirements clear in the usage error output.

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
  */
 int main(int argc, char *argv[]) {
     if (argc < 2) {
-        fprintf(stderr, "Usage: %s <packet_file>\n", argv[0]);
+        fprintf(stderr, "Usage: %s <input_file.hex|.txt>\n", argv[0]);
         return 1;
     }
 
